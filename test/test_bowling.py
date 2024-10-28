45648ae670f00458ae000e79fcf8d3a23095cce1
manhhungking <trinhmanhhung03@gmail.com>
2024-10-28 18:12:39 +0200

    test spare as the last frame game score

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -120,3 +120,18 @@ class TestBowlingGame(unittest.TestCase):
         self.game.add_frame(Frame(8, 1))
         self.game.add_frame(Frame(2, 6))
         self.assertEqual(98, self.game.calculate_score())
+
+    def test_spare_as_the_last_frame_game_score(self):
+        self.game.add_frame(Frame(1, 5))
+        self.game.add_frame(Frame(3, 6))
+        self.game.add_frame(Frame(7, 2))
+        self.game.add_frame(Frame(3, 6))
+        self.game.add_frame(Frame(4, 4))
+        self.game.add_frame(Frame(5, 3))
+        self.game.add_frame(Frame(3, 3))
+        self.game.add_frame(Frame(4, 5))
+        self.game.add_frame(Frame(8, 1))
+        f = Frame(2, 8)
+        f.set_bonus(7)
+        self.game.add_frame(f)
+        self.assertEqual(90, self.game.calculate_score())
