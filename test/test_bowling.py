1140ba508733c4a19cdb594339238c987a5b5abc
manhhungking <trinhmanhhung03@gmail.com>
2024-10-28 17:52:41 +0200

    test spare and strike game score

--- test/test_bowling.py
+++ test/test_bowling.py
@@ -81,3 +81,16 @@ class TestBowlingGame(unittest.TestCase):
         self.game.add_frame(Frame(8, 1))
         self.game.add_frame(Frame(2, 6))
         self.assertEqual(94, self.game.calculate_score())
+
+    def test_strike_and_spare_game_score(self):
+        self.game.add_frame(Frame(10, 0))
+        self.game.add_frame(Frame(4, 6))
+        self.game.add_frame(Frame(7, 2))
+        self.game.add_frame(Frame(3, 6))
+        self.game.add_frame(Frame(4, 4))
+        self.game.add_frame(Frame(5, 3))
+        self.game.add_frame(Frame(3, 3))
+        self.game.add_frame(Frame(4, 5))
+        self.game.add_frame(Frame(8, 1))
+        self.game.add_frame(Frame(2, 6))
+        self.assertEqual(103, self.game.calculate_score())
