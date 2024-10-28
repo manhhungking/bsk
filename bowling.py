fb44a8fba3ace1292400d5ba0bf84179736e9cab
manhhungking <trinhmanhhung03@gmail.com>
2024-10-28 16:58:46 +0200

    Test game created and test empty game

--- bowling.py
+++ bowling.py
@@ -1,3 +1,5 @@
+from logging import raiseExceptions
+
 from bowling_error import BowlingError
 from frame import Frame
 
@@ -5,13 +7,15 @@ from frame import Frame
 class BowlingGame:
 
     def __init__(self):
-        pass
+        self.frames = []
     
     def add_frame(self, frame: Frame) -> None:
-        pass
+        self.frames.append(frame)
 
     def get_frame_at(self, i: int) -> Frame:
-        pass
+        if i >= len(self.frames):
+            raise BowlingError
+        return self.frames[i]
 
     def calculate_score(self) -> int:
         pass
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -6,6 +6,14 @@ from frame import Frame
 
 
 class TestBowlingGame(unittest.TestCase):
+    def setUp(self):
+        self.game = BowlingGame()
 
-    def test_something(self):
-        pass
+    def test_game_created(self):
+        f = Frame(1, 5)
+        self.game.add_frame(f)
+        self.assertEqual(f, self.game.get_frame_at(0))
+
+    def test_empty_game(self):
+        self.assertRaises(BowlingError, self.game.get_frame_at, 0)
+
